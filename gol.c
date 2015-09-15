0b351b738b4bd8fb2efc60354c7319d1fecc7005
Vincent Moscatello <moscatello.vincent@gmail.com>
2015-09-14 21:01:23 -0400

    added random seed

--- gol.c
+++ gol.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define VAL 15
+#define VAL 2
+#define MIN_RANDOM 30
+#define MAX_RANDOM 50
 
 //allocate memory
 char *** alloc_memory(int x, int y){
@@ -22,14 +25,13 @@ char *** alloc_memory(int x, int y){
 }
 
 
-
 //set memory to 0
 void zero_memory(int x, int y, char *** test){
     puts("IN Z");
     for(int i=0; i<VAL; i++){
         for(int p=0; p<x; p++){
             for(int q=0; q<y; q++){
-                test[i][p][q]= (char) '0';
+                test[i][p][q]= (char) ' ';
             }
         }
     }
@@ -37,7 +39,6 @@ void zero_memory(int x, int y, char *** test){
 
 //print memory
 void print_memory(int x, int y, char *** test){
-    //print memory
     for(int i=0; i<VAL; i++){
         for(int p=0; p<x; p++){
             for(int q=0; q<y; q++){
@@ -50,19 +51,34 @@ void print_memory(int x, int y, char *** test){
     }
 }
 
+//Set value in frame t to 1
 void set_value(int t, int x, int y, char *** test){
    test[t][x][y] = '1'; 
 }
+
+//initialize first frame with random seed for game of life
+void initialize_random(int x, int y, char *** test){
+    time_t t;
+    int random_iterations;
+
+    srand((unsigned) time(&t));
+    random_iterations = (rand() % (MAX_RANDOM-MIN_RANDOM)) + MIN_RANDOM;
+    
+    for(int i=0; i<random_iterations; i++){
+        set_value(0, rand() % x, rand() % y, test);
+    }
+}
+
+void mutate(){
+
+}
+
 
 int main(void) {
     char *** test = alloc_memory(16,32);
     zero_memory(16,32,test);
     
-    for(int i=0; i<VAL; i++){
-        set_value(i, 11, 11, test);
-    }
-    
-    zero_memory(16,32,test);
+    initialize_random(16,32, test); 
 
     print_memory(16,32,test);
 	
